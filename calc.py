91161763a4bb18ba64fd775c4ac892d03ab9c62f
abd alsalam <abd.alsalam.aladawi11@gmail.com>
2020-03-31 13:01:18 +0200

    adding start project

---
+++ calc.py
@@ -0,0 +1,19 @@
+def add(x,y):
+    '''add function'''
+    return x + y
+
+def subtract(x,y):
+    '''subtract function'''
+    return x - y
+
+def multiplay(x,y):
+    '''multiplay function'''
+    return x * y
+
+def divide(x,y):
+    '''divide function'''
+    if y == 0 :
+        raise ZeroDivisionError('can not divide by zero')
+    return x + y
+print('asd')
+
